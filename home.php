23e8376c914c5e4e73313789c6e9bea88ad5d3fc
annats04 <ann_tsant@hotmail.com>
2024-10-19 12:41:54 +0200

    test

--- home.php
+++ home.php
@@ -1,45 +1,46 @@
 <?php get_header(); ?>
 <div class="container">
-    <div class="content">
-        <?php if (have_posts()): ?>
-            <?php while(have_posts()): the_post() ?>
-            
-            <?php
-            $url = get_permalink();
-            $title = get_the_title();
-            $date = get_the_date();
-            $author = get_the_author();
-            $excerpt = get_the_excerpt();
-            $mail = get_the_author_email();
-            ?>
-            
-            <!-- Author: <a href="mailto:<?php echo $mail ?>"><?php echo $author; ?></a> -->
-<div class="container">
-  <div class="row">
-    <!-- Left Column: Design Hub Title and Subtitle -->
-    <div class="col d-flex flex-column align-items-center justify-content-center">
-      <h1 class="design-hub-title text-center">DESIGN HUB</h1>
-      <h2 class="design-hub-text">Welcome to design hub</h2>
+    <!-- Design Hub Section -->
+    <div class="row my-5">
+        <!-- Left Column: Design Hub Title and Subtitle -->
+        <div class="col d-flex flex-column align-items-center justify-content-center">
+            <h1 class="design-hub-title text-center">DESIGN HUB</h1>
+            <h2 class="design-hub-text text-center">Welcome to design hub</h2>
+        </div>
     </div>
-    
-  
-  </div>
-</div>
 
-<img src="images/hero2.jpeg" alt="">
+    <!-- Hero Image -->
+    <div class="row mb-5">
+        <div class="col d-flex justify-content-center">
+            <img src="hero2.jpeg" alt="Hero Image" class="img-fluid">
+        </div>
+    </div>
 
-            <div class="post">
-                <h1><?php echo $title; ?></h1>
-                <p><?php echo $excerpt; ?></p>
-                <a href="<?php echo $url; ?>">Read more</a>
-            </div>
+    <!-- Content Section -->
+    <div class="content">
+        <div class="row">
+            <?php if (have_posts()): ?>
+                <?php while (have_posts()): the_post(); ?>
+                    <?php
+                        $url = get_permalink();
+                        $title = get_the_title();
+                        $excerpt = get_the_excerpt();
+                    ?>
 
-      
+                    <!-- Post Card -->
+                    <div class="col-md-4 mb-4">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo esc_html($title); ?></h5>
+                                <p class="card-text"><?php echo esc_html($excerpt); ?></p>
+                                <a href="<?php echo esc_url($url); ?>" class="btn btn-primary">Read more</a>
+                            </div>
+                        </div>
+                    </div>
 
-
-
-            <?php endwhile; ?>
-        <?php endif; ?>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
     </div>
 </div>
 <?php get_footer(); ?>
