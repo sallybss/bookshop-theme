a18bdc8e23466064c4b0e1a8d5783d2126cb8ecb
annats04 <ann_tsant@hotmail.com>
2024-10-19 14:26:48 +0200

    test

--- home.php
+++ home.php
@@ -43,14 +43,20 @@
         </div>
     </div>
 
-    <!-- Spotify Playlist Section -->
+   <!-- Spotify Playlist and Podcast Info Section -->
     <div class="row my-5">
-        <div class="col d-flex justify-content-center">
-            <div class="embed-responsive embed-responsive-16by9" style="max-width: 600px; width: 100%;">
+        <!-- Left Column: Podcast Info -->
+        <div class="col-lg-8 col-md-12 mb-4">
+            <h3>Check Out Our Podcasts</h3>
+            <p>Explore engaging discussions with top designers and industry leaders. In our podcasts, we cover the latest trends, challenges, and innovations in design, providing you with insights and thought-provoking conversations from experts who shape the field.</p>
+        </div>
+        
+        <!-- Right Column: Spotify Playlist -->
+        <div class="col-lg-4 col-md-12 d-flex justify-content-center">
+            <div class="embed-responsive embed-responsive-16by9" style="max-width: 400px; width: 100%;">
                 <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3SdukUsvVBUwzbS3Mv69TM?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
         </div>
     </div>
 </div>
 <?php get_footer(); ?>
-
