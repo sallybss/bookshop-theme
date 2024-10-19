ca70c82cea8e808c1d0660e193e6b9e57102c740
annats04 <ann_tsant@hotmail.com>
2024-10-19 12:26:33 +0200

    test

--- home.php
+++ home.php
@@ -17,13 +17,13 @@
 <div class="container">
   <div class="row">
     <!-- Left Column: Design Hub Title and Subtitle -->
-    <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
+    <div class="col-lg-8 d-flex flex-column align-items-center justify-content-center">
       <h1 class="design-hub-title">DESIGN HUB</h1>
       <h2 class="design-hub-text">Welcome to design hub</h2>
     </div>
     
     <!-- Right Column: Hero Image -->
-    <div class="col-lg-6 d-flex">
+    <div class="col-lg-4 d-flex">
       <img src="<?php echo $heroimage['url']; ?>" alt="Hero Image" class="img-fluid" width="650">
     </div>
   </div>
