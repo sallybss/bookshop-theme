93f9bb385587ec62bc60e1671cd0ebdb22274330
annats04 <ann_tsant@hotmail.com>
2024-10-19 12:04:54 +0200

    test

--- home.php
+++ home.php
@@ -15,17 +15,20 @@
             
             <!-- Author: <a href="mailto:<?php echo $mail ?>"><?php echo $author; ?></a> -->
 <div class="container">
-    <div class="row">
-            <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
-        <h1 class="design-hub-title"> DESIGN HUB </h1>
-        <h2 class="design-hub-text"> Welcome to design hub</h2>
-            </div>
-            
-                <div class="col-lg-6 heroimage text-end">
-             <img src="<?php echo $heroimage["url"]; ?>" alt="" class="img-fluid heroimage" width="650">
-                </div>
+  <div class="row">
+    <!-- Left Column: Design Hub Title and Subtitle -->
+    <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center design-hub-container">
+      <h1 class="design-hub-title">DESIGN HUB</h1>
+      <h2 class="design-hub-text">Welcome to design hub</h2>
     </div>
+    
+    <!-- Right Column: Hero Image -->
+    <div class="col-lg-6 heroimage text-end">
+      <img src="<?php echo $heroimage['url']; ?>" alt="Hero Image" class="img-fluid" width="650">
+    </div>
+  </div>
 </div>
+
             <div class="post">
                 <h1><?php echo $title; ?></h2>
                 <p><?php echo $excerpt; ?></p>
