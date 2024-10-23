b133bab551110260d667a5c96d8d93dd9f4737e9
sallybss <salbse01@easv365.dk>
2024-10-23 19:34:16 +0200

    about try

--- page-about.php
+++ page-about.php
@@ -8,15 +8,16 @@ get_header();
 if (have_posts()) :
     while (have_posts()) : the_post();
 
-        // Declare ACF fields as variables
-        $about_image = get_field('about_image');
+
+        $about_image = get_field('about_img');
         $about_text = get_field('about_text');
         ?>
 
         <div class="about-page">
             <img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" class="about-image">
+
             <div class="about-text">
-                <h1>About Us</h1>
+            <h1><?php echo $about_text; ?></h1>
                 <p><?php echo $about_text; ?></p>
             </div>
         </div>
