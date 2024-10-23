f35880723279e2e08f144dc2149225cb1cf5af16
annats04 <ann_tsant@hotmail.com>
2024-10-23 12:26:56 +0200

    test

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
 <?php get_header() ?>
 
-
-
-
-
-
-
-
 <?php if (have_posts()): ?>
     <?php while (have_posts()): the_post(); ?>
-
-<?php
-$loop = new WP_Query(array(
-    'post_type' => 'product',
-    'posts_per_page' => -1
-));
-?>
-
-
-
 
     <?php 
     $heroimage = get_field("hero-img");
@@ -35,25 +18,22 @@ $loop = new WP_Query(array(
     $buttonexplore = get_field("button_explore");
     $trustpilot = get_field("trustpilot");
     $btntext = get_field("button_text");
-
     ?>
 
+    <img src="<?php echo $heroimage["url"]; ?>" alt="" class="hero" >
 
-<img src="<?php echo $heroimage["url"]; ?>" alt="" class="hero" >
+    <div class="text-hero">
+        <h1><?php echo $herotext; ?></h1>
+        <p><?php echo  $intro; ?></p>
+    </div>
 
-<div class="text-hero">
-    <h1><?php echo $herotext; ?></h1>
-    <p><?php echo  $intro; ?></p>
-</div>
-
-
-<div class="grey-section">
+    <div class="grey-section">
         <div class="content">
             <div class="image">
-            <img src="<?php echo $desc["url"]; ?>" alt="Section Image" class="image" >
+                <img src="<?php echo $desc["url"]; ?>" alt="Section Image" class="image" >
             </div>
             <div class="text">
-            <p><?php echo  $desctext; ?></p>
+                <p><?php echo  $desctext; ?></p>
             </div>
         </div>
     </div>
@@ -65,34 +45,67 @@ $loop = new WP_Query(array(
                 <p><?php echo  $saletext; ?></p>
             </div>
             <div class="small-text">
-            <p><?php echo  $saletext1; ?></p>
+                <p><?php echo  $saletext1; ?></p>
             </div>
         </div>
     </div>
 
-
     <div class="pink-section">
         <div class="content-pink">
             <div class="image">
-            <img src="<?php echo $exploreimage["url"]; ?>" alt="Books">
+                <img src="<?php echo $exploreimage["url"]; ?>" alt="Books">
             </div>
             <div class="text">
-               <h1><?php echo  $exploreh1; ?></h1>
-               <p><?php echo  $exploretext; ?></p>
-                <button><a href= "<?php echo $buttonexplore ?>"target="_blank"><?php echo  $btntext; ?></a></button>
+                <h1><?php echo  $exploreh1; ?></h1>
+                <p><?php echo  $exploretext; ?></p>
+                <button><a href="<?php echo $buttonexplore ?>" target="_blank"><?php echo  $btntext; ?></a></button>
             </div>
         </div>
     </div>
 
     <div class="centered-image">
-    <img src="<?php echo $trustpilot["url"]; ?>" alt="Trustpilot rate">
+        <img src="<?php echo $trustpilot["url"]; ?>" alt="Trustpilot rate">
     </div>
-
-
 
     <?php endwhile; ?>
 <?php endif; ?>
 
+<!-- WooCommerce Product Loop Section -->
+<div class="woocommerce-section">
+    <h2>Our Book Collection</h2>
+    <div class="products-wrapper">
+        <?php
+        $loop = new WP_Query(array(
+            'post_type' => 'product',
+            'posts_per_page' => -1
+        ));
+        ?>
 
+        <?php if($loop->have_posts()): ?>
+            <?php while ($loop->have_posts()) : $loop->the_post(); ?>
 
-<?php get_footer(); ?>
+                <?php
+                global $product;
+                $title = get_the_title();
+                $excerpt = get_the_excerpt();
+                $price = $product->get_price_html();
+                $link = get_the_permalink();
+                $image = get_the_post_thumbnail_url($product->get_id(), 'medium');
+                ?>
+
+                <div class="product-card">
+                    <a href="<?php echo $link; ?>">
+                        <img src="<?php echo $image; ?>" alt="<?php echo $title; ?>" class="product-image">
+                        <h3 class="product-title"><?php echo $title; ?></h3>
+                        <p class="product-price"><?php echo $price; ?></p>
+                        <p class="product-excerpt"><?php echo $excerpt; ?></p>
+                    </a>
+                </div>
+
+            <?php endwhile; ?>
+        <?php endif; ?>
+        <?php wp_reset_postdata(); ?>
+    </div>
+</div>
+
+<?php get_footer() ?>
