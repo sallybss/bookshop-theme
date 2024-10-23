1e08a13b7b3c0aeb52235e40f43894e57f30d32e
annats04 <ann_tsant@hotmail.com>
2024-10-23 17:10:43 +0200

    test about

--- page-about.php
+++ page-about.php
@@ -1,22 +1,28 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: About Page
+*/
 
-<?php if (have_posts()): ?>
-    <?php while (have_posts()): the_post(); ?>
+get_header();
 
-    <?php 
-    $about_image = get_field("about_image");
-    $about_text = get_field("about_text");
-    ?>
+if (have_posts()) :
+    while (have_posts()) : the_post();
 
-    <div class="about-page">
-        <img src="<?php echo $about_image['url']; ?>" alt="About Us" class="about-image">
-        <div class="about-text">
-            <h1>About Us</h1>
-            <p><?php echo $about_text; ?></p>
+        // Declare ACF fields as variables
+        $about_image = get_field('about_image');
+        $about_text = get_field('about_text');
+        ?>
+
+        <div class="about-page">
+            <img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" class="about-image">
+            <div class="about-text">
+                <h1>About Us</h1>
+                <p><?php echo $about_text; ?></p>
+            </div>
         </div>
-    </div>
 
-    <?php endwhile; ?>
-<?php endif; ?>
+    <?php endwhile;
+endif;
 
-<?php get_footer(); ?>
+get_footer();
+?>
