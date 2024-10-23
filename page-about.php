cbb031362cda7f8a56b29223df987afeb6d447f5
sallybss <salbse01@easv365.dk>
2024-10-23 19:39:13 +0200

    about try2

--- page-about.php
+++ page-about.php
@@ -8,24 +8,22 @@ get_header();
 if (have_posts()) :
     while (have_posts()) : the_post();
 
-
         $about_image = get_field('about_img');
         $about_text = get_field('about_text');
         ?>
 
         <div class="about-page">
-            <img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" class="about-image">
-
-            <div class="about-text">
-            <h1><?php echo $about_text; ?></h1>
-                <p><?php echo $about_text; ?></p>
+            <div class="about-content">
+                <div class="about-text">
+                    <h1><?php echo $about_text; ?></h1>
+                    <p><?php echo $about_text; ?></p>
+                </div>
+                <div class="about-image">
+                    <img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>" class="about-image">
+                </div>
             </div>
         </div>
 
-
-        <h1>
-            HELLOW WORLD
-        </h1>
     <?php endwhile;
 endif;
 
