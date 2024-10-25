2cc1db9bbed5c5168d142a6ebc34a151372d54e4
annats04 <ann_tsant@hotmail.com>
2024-10-25 10:03:52 +0200

    test

--- footer.php
+++ footer.php
@@ -15,46 +15,53 @@ $about = get_field("about");
 ?>
 
 <footer>
+    <div class="footer-container container">
+        <div class="row">
+            <div class="col-1">
+                <div class="footer-column footer-logo">
+                    <img src="<?php echo $logoimg['url']; ?>" alt="Company Logo">
+                </div>
+            </div>
 
-<hr>
+            <div class="col-2">
+                <div class="footer-column connect">
+                    <h3><?php echo $footercol1h1; ?></h3>
+                    <ul>
+                        <li><a href="<?php echo $insta; ?>" target="_blank">Instagram</a></li>
+                        <li><a href="<?php echo $fb; ?>" target="_blank">Facebook</a></li>
+                        <li><a href="<?php echo $threads; ?>" target="_blank">Threads</a></li>
+                    </ul>
+                </div>
+            </div>
 
-    <div class="footer-container">
-        <div class="footer-column connect">
-            <h3><?php echo $footercol1h1; ?></h3>
-            <ul>
-            <li><a href="<?php echo $insta; ?>" target="_blank"> Instagram </a></li>
-            <li><a href="<?php echo $fb; ?>" target="_blank"> Facebook </a></li>
-            <li><a href="<?php echo $threads; ?>" target="_blank"> Threads </a></li>
-            </ul>
+            <div class="col-2">
+                <div class="footer-column company">
+                    <h3><?php echo $footercol1h2; ?></h3>
+                    <ul>
+                        <li><a href="<?php echo $about; ?>" target="_blank"><?php echo $hours; ?></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="footer-column support">
+                    <h3><?php echo $footercol1h3; ?></h3>
+                    <ul>
+                        <li><a href="<?php echo $address; ?>" target="_blank"><?php echo $location; ?></a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-4">
+                <div class="footer-column newsletter">
+                    <h3><?php echo $footercol1h4; ?></h3>
+                    <?php echo do_shortcode('[contact-form-7 id="d878031" title="Contact form 1"]'); ?>
+                </div>
+            </div>
         </div>
-        
-        <div class="footer-column company">
-            <h3><?php echo $footercol1h2; ?></h3>
-            <ul>
-            <li><a href="<?php echo $about; ?>" target="_blank"><?php echo  $hours; ?></a></li>
-            </ul>
-        </div>
-        
-        <div class="footer-column support">
-            <h3><?php echo $footercol1h3; ?></h3>
-            <ul>
-            <li><a href="<?php echo $address; ?>" target="_blank"> <?php echo  $location; ?></a></li>
-
-            </ul>       
-        </div>
-
-        <div class="footer-column newsletter">
-    <h3><?php echo $footercol1h4; ?></h3>
-    
-    <?php echo do_shortcode('[contact-form-7 id="d878031" title="Contact form 1"]'); ?>
-    
-</div>
+        <hr>
     </div>
-    
-    <hr>
-   <div class="footer-logo">
-        <img src="<?php echo $logoimg["url"]; ?>" alt="Company Logo">
-    </div>
+    <?php wp_footer(); ?>
 </footer>
 
 
