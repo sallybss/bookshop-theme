ef71ef60ab1f8e6e45872065737b0973c236dd11
annats04 <ann_tsant@hotmail.com>
2024-10-26 23:29:57 +0200

    contact page

---
+++ page-contact.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: Contact
+*/
+
+
+get_header();     ?>
+
+<?php if (have_posts()) : ?>
+    <?php  while (have_posts()) : the_post(); ?>
+
+    <?php 
+        $about_image = get_field('about_img');
+        $about_text = get_field('about_text');
+        $about_description = get_field('about_description');
+        ?>
+
+        <div class="about-page">
+            <div class="about-content">
+
+                <div class="about-text">
+                    <h1><?php echo $about_text; ?></h1>
+                    <p><?php echo $about_description; ?></p>
+                </div>
+
+                <div class="about-image">
+                    <img src="<?php echo $about_image['url']; ?>" alt="<?php echo $about_image['alt']; ?>">
+                </div>
+            </div>
+        </div>
+     <?php endwhile; ?>
+   <?php endif; ?>
+
+
+   <?php get_footer() ?>
