0146d8a85cb44204c60d42de4f7caadfcd0f987e
annats04 <ann_tsant@hotmail.com>
2024-10-26 23:45:29 +0200

    test

--- page-contact.php
+++ page-contact.php
@@ -10,24 +10,26 @@ get_header();     ?>
     <?php  while (have_posts()) : the_post(); ?>
 
 
-<div class="form-container">
-    <h1>GET IN TOUCH</h1>
-    <div class="form">
-        <?php echo do_shortcode('[cf7form cf7key="form"]'); ?>  
-    </div>
+<div class="container">
+    <div class="row">
+            <div class="col-6">
+            <h1>GET IN TOUCH</h1>
+            <div class="form">
+        
+            <div class="form">
+                <?php echo do_shortcode('[cf7form cf7key="form"]'); ?>  
+            </div>
+            </div>
+            </div>
+
+<div class="col-6">
+<div>FOLLOW US</div>
+<div> never miss out on events and offers on Instagram @avrktil</div>
+<div>image </div>
 </div>
 
-<div class="container follow">
-<div class="row">
-    <div class="col-4">
-        Keep in touch by following us on Instagram and get updated about events, offers, news and more!
-    </div>
-    <div class="col-4">
-        image
-    </div>
 </div>
 </div>
-
      <?php endwhile; ?>
    <?php endif; ?>
 
